dee8c98e496c820bc4d4d2186a2848f2701a7ef5
James-yaoshenglong <yaoshenglong758063348@gmail.com>
2021-02-02 22:12:06 +0800

    add the HandCard status pattern(not decided)

---
+++ BasicGame/src/Battle/CardStatus.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Battle;
+
+/**
+ *
+ * @author 影音娱乐剪辑
+ */
+public interface CardStatus {
+    void onClickAction();
+}
---
+++ BasicGame/src/Battle/HandCardStatus.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Battle;
+
+/**
+ *
+ * @author 影音娱乐剪辑
+ */
+public class HandCardStatus implements CardStatus{
+
+    @Override
+    public void onClickAction() {
+        
+    }
+    
+}
