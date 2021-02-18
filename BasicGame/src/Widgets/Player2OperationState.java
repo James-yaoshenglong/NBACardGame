5a271bb8d2656f9afd035a792167b22604676e88
James-yaoshenglong <yaoshenglong758063348@gmail.com>
2021-02-18 16:47:30 +0800

    add the player1,2 button state pattern

---
+++ BasicGame/src/Widgets/Player2OperationState.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Widgets;
+
+/**
+ *
+ * @author shenglyao2
+ */
+public class Player2OperationState implements ButtonState{
+
+    @Override
+    public void showActionButton() {
+    }
+
+    @Override
+    public void showCards() {
+    }
+
+    @Override
+    public void changePlayerButton() {
+    }
+
+    @Override
+    public void changeOperationBUtton() {
+    }
+
+    
+}
