a9bac593df6e2d365ee78d70d6d2cd999b55dbee
DELTA-DoubleWise <deltawang7@gmail.com>
2021-02-22 00:17:23 +0800

    add the value display widget

--- BasicGame/src/Battle/BattleValueSphere.java
+++ BasicGame/src/Battle/BattleValueSphere.java
@@ -13,12 +13,16 @@ import com.atr.jme.font.shape.TrueTypeNode;
 import com.atr.jme.font.util.Style;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
+import com.jme3.material.RenderState;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Dome;
+import com.jme3.scene.shape.Quad;
 import com.jme3.scene.shape.Sphere;
+import com.jme3.texture.Texture;
 
 /**
  *
@@ -41,17 +45,26 @@ public class BattleValueSphere extends Node{
     }
     
     private void initialize(){
-        sphereGeom = new Geometry("hhh", new Sphere(20,20,width/3));
-        Material mat = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        mat.getAdditionalRenderState().setWireframe(true);
+//        sphereGeom = new Geometry("hhh", new Sphere(20,20,width/3));
+//        Material mat = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+//        mat.setColor("Color", ColorRGBA.Blue);
+//        mat.getAdditionalRenderState().setWireframe(true);
+//        sphereGeom.setMaterial(mat);
+        Quad quad = new Quad(width/3,width/3);
+        sphereGeom = new Geometry("value_display",quad);
+        Texture tex = app.getAssetManager().loadTexture("Textures/pic/valuedisplay.png");
+        Material mat = new Material(app.getAssetManager(),"Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setTexture("ColorMap", tex);
         sphereGeom.setMaterial(mat);
-        
+        mat.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
+        sphereGeom.setQueueBucket(RenderQueue.Bucket.Transparent); 
+        sphereGeom.setLocalTranslation(-width/6,0,0.1f);
+
         app.getAssetManager().registerLoader(TrueTypeLoader.class, "ttf");
-        TrueTypeKeyMesh ttk = new TrueTypeKeyMesh("Fonts/arial.ttf", Style.Plain,2);
+        TrueTypeKeyMesh ttk = new TrueTypeKeyMesh("Fonts/arial.ttf", Style.Plain,5);
         TrueTypeFont font = (TrueTypeMesh)(app.getAssetManager()).loadAsset(ttk);
-        ttkFontNode = font.getText(value.toString(), 0, ColorRGBA.Black);
-        ttkFontNode.setLocalTranslation(-width/10, width/10, 2f);
+        ttkFontNode = font.getText(value.toString(), 0, ColorRGBA.White);
+        ttkFontNode.setLocalTranslation(-width/8, width*(7f/24f), 0.2f);
         
         this.attachChild(sphereGeom);
         this.attachChild(ttkFontNode);
